4c2ef067910d9dd035650e3ed9b8ac7e10122018
finest-cy <73606411+finest-cy@users.noreply.github.com>
2021-04-03 01:24:20 +0800

    Update OwO.js

--- Assets/fontend/OwO/OwO.js
+++ Assets/fontend/OwO/OwO.js
@@ -1,3 +1,9 @@
+var jQ = jQuery.noConflict();
+
+if(/^https:\/\/finest-cy\.com.*/i.test(window.location.href)){
+  function _classCallCheck(e, t) {
+	if (! (e instanceof t)) throw new TypeError("Cannot call a class as a function");
+}
 var OwO_demo = new OwO({
         logo: '表情',
         container: document.getElementsByClassName('OwO')[0],
@@ -7,3 +13,6 @@ var OwO_demo = new OwO({
         width: '100%',
         maxHeight: '250px'
     });
+}else{
+	document.write("<h1>人作死,就会死</h1>");
+}
